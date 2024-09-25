249db8176e1e4c03423259f6e4219b03a3288144
nicolasvalderrama689 <nicolasvalderrama689@gmail.com>
2024-09-25 14:46:40 -0500

    Ejercicios go rama develop

---
+++ ejercicio2.go
@@ -0,0 +1,19 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("texto de la funcion ejercicio 2")
+// }
+
+// func arreglo() {
+// 	var aprendices [5]string
+
+// 	aprendices[0] = "Camilo"
+// 	aprendices[1] = "Carlos"
+// 	aprendices[2] = "David"
+// 	aprendices[3] = "Esteban"
+// 	aprendices[4] = "Nicolas"
+
+// 	fmt.Println(aprendices[2])
+// }
